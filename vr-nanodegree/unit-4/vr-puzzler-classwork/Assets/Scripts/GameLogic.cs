59ff150a821eac7ebd9a5029603086d167a2b3b2
Heath Garvey <heathgarvey@gmail.com>
2017-02-07 07:52:19 +1100

    Track Unit Four directory

---
+++ vr-nanodegree/unit-4/vr-puzzler-classwork/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+	public GameObject player;
+	public GameObject startUI, restartUI;
+	public GameObject startPoint, playPoint, restartPoint;
+
+	public bool playerWon = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player.transform.position = startPoint.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0) && player.transform.position==playPoint.transform.position) {
+			puzzleSuccess ();
+		}
+	}
+
+	//Begin the puzzle sequence
+	public void startPuzzle()
+	{
+		toggleUI();
+		iTween.MoveTo (player, iTween.Hash 
+			(
+				"position", playPoint.transform.position, 
+				"time", 2, 
+				"easetype", "linear"
+			)
+		);
+
+	}
+
+	//Reset the puzzle sequence
+	public void resetPuzzle()
+	{
+		player.transform.position = startPoint.transform.position;
+		toggleUI ();
+	}
+
+	//Do this when the player gets it right
+	public void puzzleSuccess()
+	{
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", restartPoint.transform.position, 
+				"time", 2, 
+				"easetype", "linear"
+			)
+		);
+	}
+	public void toggleUI()
+	{
+		startUI.SetActive (!startUI.activeSelf);
+		restartUI.SetActive (!restartUI.activeSelf);
+	}
+
+}
