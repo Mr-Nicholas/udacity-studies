f4875804ec4313664f31e598da81794fc1a2cc55
Heath Garvey <heathgarvey@gmail.com>
2017-06-29 00:41:28 +1000

    Profanity editor update

--- python-intro/profanity-editor/profanity-editor.py
+++ python-intro/profanity-editor/profanity-editor.py
@@ -1,3 +1,5 @@
+import urllib
+
 ''' This simple program reads a .txt file and removes common profanities'''
 
 # save a list of profane words
@@ -16,4 +18,19 @@ def read_txt():
     file_contents = quotes.read()
     print(file_contents)
     quotes.close()
+    test_profanity(file_contents)
+
+def test_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+   # print(output)
+    connection.close()
+
+    if "true" in output:
+        print("Profanity has been detected.")
+    elif "false" in output:
+        print("No profanity has been found.")
+    else:
+        print("Error in scanning file provided.")
+    
 read_txt()
